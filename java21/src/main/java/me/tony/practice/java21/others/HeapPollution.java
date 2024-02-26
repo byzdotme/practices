f21822cd3ddcc6ed67bddbdad575d42cda40c3f2
zhubaiyang <zhubaiyang@meituan.com>
2024-02-26 15:20:03 +0800

    ...

--- java21/src/main/java/me/tony/practice/java21/others/HeapPollution.java
+++ java21/src/main/java/me/tony/practice/java21/others/HeapPollution.java
@@ -2,26 +2,26 @@ package me.tony.practice.java21.others;
 
 public class HeapPollution {
 
-    public static void main(String[] args) {
-        HeapPollution heapPollution = new HeapPollution();
-        System.out.println(heapPollution.bar("Hello"));
-        String[] arr = heapPollution.foo2("Hello");
-        System.out.println(arr[0]);
-    }
-
-    <T> String foo(T... arr) {
-        return arr.getClass().getComponentType().getName();
-    }
-
-    <T> String bar(T obj) {
-        return foo(obj);
-    }
-
-    <T> T[] foo1(T... arr) {
-        return arr;
-    }
-
-    <T> T[] foo2(T obj) {
-        return foo1(obj);
-    }
+//    public static void main(String[] args) {
+//        HeapPollution heapPollution = new HeapPollution();
+//        System.out.println(heapPollution.bar("Hello"));
+//        String[] arr = heapPollution.foo2("Hello");
+//        System.out.println(arr[0]);
+//    }
+//
+//    <T> String foo(T... arr) {
+//        return arr.getClass().getComponentType().getName();
+//    }
+//
+//    <T> String bar(T obj) {
+//        return foo(obj);
+//    }
+//
+//    <T> T[] foo1(T... arr) {
+//        return arr;
+//    }
+//
+//    <T> T[] foo2(T obj) {
+//        return foo1(obj);
+//    }
 }
